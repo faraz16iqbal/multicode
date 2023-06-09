47f89053b99f65d2d7a9101f49f5bea3cc22eb3f
Faraz Iqbal <faraziqbal@hotmail.co.uk>
2023-06-09 10:25:52 +0530

    Fix CORS bug

--- server/index.ts
+++ server/index.ts
@@ -21,7 +21,7 @@ require('dotenv').config();
 
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Credentials", true);
+	res.header("Access-Control-Allow-Credentials", "true");
 	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
 	res.header(
 		"Access-Control-Allow-Headers",
